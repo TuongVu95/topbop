53fda762f9d3484bbd312023506deb1758bfb5fa
nguyên <72590935+doannhatnguyen97@users.noreply.github.com>
2022-03-10 11:11:42 +0700

    update

--- js/function.js
+++ js/function.js
@@ -1,26 +1,11 @@
-const slideBestSellar = () => {
-    const groupItem = 5;
-    var swiper = new Swiper(".jsBestseller", {
-      spaceBetween: 30,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
+const topbopSwiper = new Swiper('.topbop__swiper', {
+  spaceBetween: 16,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+})
 
-        breakpoints: {
-          320: {
-            slidesPerView: parseInt(groupItem - 3),
-            slidesPerGroup: parseInt(groupItem - 3),
-          },
-          768: {
-            slidesPerView: groupItem,
-            slidesPerGroup: groupItem,
-
-          },
-        },
-      });
-}
 const init = () => {
-    slideBestSellar();
 }
 init();
