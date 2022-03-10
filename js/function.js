77442621c3476b81e5eb70432308f6286f797ecb
nguyên <72590935+doannhatnguyen97@users.noreply.github.com>
2022-03-10 13:39:40 +0700

    update

--- js/function.js
+++ js/function.js
@@ -1,5 +1,7 @@
 const topbopSwiper = new Swiper('.topbop__swiper', {
+  slidesPerView: 3.1,
   spaceBetween: 16,
+  slidesPerGroup: 3,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
